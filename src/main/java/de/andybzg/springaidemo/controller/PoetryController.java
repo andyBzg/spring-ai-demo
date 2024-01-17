0b30e2ce3814548a3cf8a968ad7aeca558b9f67a
andyBzg <andr.bzg@gmail.com>
2024-01-17 16:03:08 +0100

    add code from previous commit, create README.md

--- src/main/java/de/andybzg/springaidemo/controller/PoetryController.java
+++ src/main/java/de/andybzg/springaidemo/controller/PoetryController.java
@@ -1,2 +1,34 @@
-package de.andybzg.springaidemo.controller;public class PoetryController {
+package de.andybzg.springaidemo.controller;
+
+import de.andybzg.springaidemo.dto.PoetryDto;
+import de.andybzg.springaidemo.service.PoetryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ai")
+public class PoetryController {
+
+    private final PoetryService poetryService;
+
+    @Autowired
+    public PoetryController(PoetryService poetryService) {
+        this.poetryService = poetryService;
+    }
+
+    @GetMapping("/cathaiku")
+    public ResponseEntity<String> generateHaiku() {
+        return ResponseEntity.ok(poetryService.getCatHaiku());
+    }
+
+    @GetMapping("/poetry")
+    public ResponseEntity<PoetryDto> generatePoetry(
+            @RequestParam("genre") String genre,
+            @RequestParam("theme") String theme) {
+        return ResponseEntity.ok(poetryService.getPoetryByGenreAndTheme(genre, theme));
+    }
 }
--- src/main/java/de/andybzg/springaidemo/service/impl/PoetryServiceImpl.java
+++ src/main/java/de/andybzg/springaidemo/service/impl/PoetryServiceImpl.java
@@ -1,2 +1,51 @@
-package de.andybzg.springaidemo.service.impl;public class PoetryServiceImpl {
+package de.andybzg.springaidemo.service.impl;
+
+import de.andybzg.springaidemo.dto.PoetryDto;
+import de.andybzg.springaidemo.service.PoetryService;
+import org.springframework.ai.client.AiClient;
+import org.springframework.ai.client.AiResponse;
+import org.springframework.ai.parser.BeanOutputParser;
+import org.springframework.ai.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PoetryServiceImpl implements PoetryService {
+
+    public static final String WRITE_ME_HAIKU_ABOUT_CAT = """
+            Write me Haiku about cat,
+            haiku should start with the word cat obligatory
+            """;
+
+    private final AiClient aiClient;
+
+    @Autowired
+    public PoetryServiceImpl(AiClient aiClient) {
+        this.aiClient = aiClient;
+    }
+
+    @Override
+    public String getCatHaiku() {
+        return aiClient.generate(WRITE_ME_HAIKU_ABOUT_CAT);
+    }
+
+    @Override
+    public PoetryDto getPoetryByGenreAndTheme(String genre, String theme) {
+        BeanOutputParser<PoetryDto> poetryDtoBeanOutputParser = new BeanOutputParser<>(PoetryDto.class);
+
+        String promptString = """
+                Write me {genre} poetry about {theme}
+                {format}
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(promptString);
+        promptTemplate.add("genre", genre);
+        promptTemplate.add("theme", theme);
+        promptTemplate.add("format", poetryDtoBeanOutputParser.getFormat());
+        promptTemplate.setOutputParser(poetryDtoBeanOutputParser);
+
+        AiResponse response = aiClient.generate(promptTemplate.create());
+
+        return poetryDtoBeanOutputParser.parse(response.getGeneration().getText());
+    }
 }
